f956d10471193bf6ee47a182ca2538afe8657cfe
Doomslug <cbarrett17@georgefox.edu>
2023-04-15 10:41:20 -0700

    added interior piece classes

--- src/game/GamePiece.java
+++ src/game/GamePiece.java
@@ -2,3 +2,21 @@ package game;
 
 public abstract class GamePiece {
 }
+
+class King extends GamePiece {
+}
+
+class Queen extends GamePiece {
+}
+
+class Bishop extends GamePiece {
+}
+
+class Knight extends GamePiece {
+}
+
+class Rook extends GamePiece {
+}
+
+class Pawn extends GamePiece {
+}
